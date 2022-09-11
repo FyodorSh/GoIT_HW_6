fc816432a035a71822334ba112d05051182749f5
Fyodor Shevchenko <fyodor.shevchenko@gmail.com>
2022-09-11 17:39:09 +0300

    Pre-finally

--- main.py
+++ main.py
@@ -68,7 +68,9 @@
 import re
 import sys
 import os
-from pprint import pprint
+import zipfile
+import py7zr
+import patoolib
 
 CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
 TRANSLATION = (
@@ -81,9 +83,8 @@ EXTENSIONS = {('JPEG', 'PNG', 'JPG', 'SVG'): "images", ('AVI', 'MP4', 'MOV', 'MK
               ('DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX'): "documents",  ('MP3', 'OGG', 'WAV', 'AMR'): "audio",
               ('ZIP', 'RAR', '7ZIP', '7Z', 'GZ', 'TAR'): "archives"}
 
-SORT_DIRS = dict()
-
-FILES = {}
+SORT_DIRS = {}
+SORT_DIRS_LIST = []
 
 for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
     TRANS[ord(c)] = l
@@ -101,38 +102,64 @@ def normalize(file_name: str):
     return file_name
 
 
-def check_dir(current_path: str):
+def check_dir(current_path: str, if_empty_delete=False):
     for file in os.listdir(current_path):
         file_path = os.path.join(current_path, file)
+        if file_path in SORT_DIRS_LIST:
+            continue
+
         if os.path.isdir(file_path):
-            check_dir(file_path)
+            if len(os.listdir(file_path)) == 0:
+                if if_empty_delete:
+                    os.removedirs(file_path)
+                    continue
+            else:
+                check_dir(file_path)
+                continue
+        file_name_split = os.path.splitext(file)
+        file_ext = file_name_split[1].replace(".", "").upper()
 
-        file_ext = os.path.splitext(file)[1].replace(".", "").upper()
-
-        directory = ""
         for ext in EXTENSIONS:
             if file_ext in ext:
                 directory = EXTENSIONS.get(ext)
-                os.replace(file_path, os.path.join(SORT_DIRS.get(directory), normalize(file)))
-        # pprint(translit(file_path) + " ---> " + directory)
+                sort_dir = SORT_DIRS.get(directory)
+                new_file_name = os.path.join(sort_dir, normalize(file))
+                os.replace(file_path, new_file_name)
+                if directory == "archives":
+                    try:
+                        extract_path = os.path.join(sort_dir, normalize(file_name_split[0]))
+                        if file_ext == "7Z":
+                            archive = py7zr.SevenZipFile(new_file_name, mode='r')
+                            archive.extractall(path = extract_path)
+                            archive.close()
+                        elif file_ext == "RAR":
+                            patoolib.extract_archive(new_file_name, outdir=extract_path, interactive=False)
+                        else:
+                            with zipfile.ZipFile(new_file_name, 'r') as zip_ref:
+                                zip_ref.extractall(extract_path)
+                        os.remove(new_file_name)
+                        print(f"{new_file_name} extracted")
+                    except:
+                        print(f"{file} not zip archive")
+                else:
+                    print(f'"{file}" removed and renamed to folder "{directory}"')
 
 
-def create_sort_dir(start_path):
+def create_sort_dir(start_path: str):
     for ext in EXTENSIONS:
         sort_dir = EXTENSIONS.get(ext)
         sort_dir_path = os.path.join(start_path, sort_dir)
         if not os.path.exists(sort_dir_path):
             os.mkdir(sort_dir_path)
         SORT_DIRS.update({sort_dir: sort_dir_path})
+        SORT_DIRS_LIST.append(sort_dir_path)
 
-def main(start_path):
+
+def main(start_path: str):
     create_sort_dir(start_path)
     check_dir(start_path)
-    # if not os.path.exists(os.path.join(path,"images")):
-    #     os.makedirs(os.path.join(path,"images"))
-    # os.replace(old, new)
-    # os.remove(old)
-
+    check_dir(start_path, True) # delete empty folders
+    print("Sorting complete.")
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
